4d18e973090d623bf2ff091ccbc592c52554fd3c
kimjiwoon3472 <25_17@bssm.hs.kr>
2026-08-25 19:20:14 +0900

    chore(docker-compose.yml, application.properties): 도커 빌드 설정
    - docker-compose.yml 파일 작성
    - application.properties 파일 수정
    - build.gradle postgresql 의존성 추가
    
    Refs: #000

--- src/main/java/msa/user/msachatuser/entity/User.java
+++ src/main/java/msa/user/msachatuser/entity/User.java
@@ -1,9 +1,6 @@
 package msa.user.msachatuser.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @Getter
+@Table(name = "users")
 public class User {
 
     @Id
